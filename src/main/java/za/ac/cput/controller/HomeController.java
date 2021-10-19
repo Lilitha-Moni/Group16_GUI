99bb83a8904e6d96f88381d93f2d5008446a5775
FlamboD <FlamboD2000@gmail.com>
2021-10-19 23:12:52 +0200

    initial commit

---
+++ src/main/java/za/ac/cput/controller/HomeController.java
@@ -0,0 +1,27 @@
+package za.ac.cput.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController
+{
+    @RequestMapping({"/", "/home"})
+    public String home()
+    {
+        return "home";
+    }
+
+    @RequestMapping({"/dashboard"})
+    public String dashboard()
+    {
+        return "dashboard";
+    }
+
+
+    @RequestMapping( "/login")
+    public String login()
+    {
+        return "login";
+    }
+
+}
