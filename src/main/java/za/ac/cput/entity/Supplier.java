4eb6eafcf5080d68836dfb6c0f47e09527478077
AkhonaMngqibisa <bakhonamngqibisa@gmail.com>
2021-10-20 00:22:01 +0200

    Added some needed packages for GUI and edited application.prop

---
+++ src/main/java/za/ac/cput/entity/Supplier.java
@@ -0,0 +1,88 @@
+package za.ac.cput.entity;
+
+/* Supplier.java
+Entity for the Supplier
+Author: Lilitha Moni (219068429)
+Date: 10 June 2021
+*/
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "Supplier")
+public class Supplier {
+    @Id
+    private int supId;
+
+    private int eqId;
+    private String supName;
+
+
+    private Supplier(Builder builder) {
+        this.supId = builder.supId;
+        this.supName = builder.supName;
+        this.eqId = builder.eqId;
+
+
+    }
+
+    public Supplier() {
+    }
+
+    public int getSupId() {
+        return supId;
+    }
+
+    public String getSupName() {
+        return supName;
+    }
+
+    public int getEqId() {
+        return eqId;
+    }
+
+    @Override
+    public String toString() {
+        return "Supplier{" +
+                "Supplier Id ='" + supId + '\'' +
+                ", Name ='" + supName + '\'' +
+                ", Equipment Id ='" + eqId + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private int supId, eqId;
+        private String supName;
+
+        public Builder setSupId(int supId) {
+            this.supId = supId;
+            return this;
+        }
+
+        public Builder setSupName(String supName) {
+            this.supName = supName;
+            return this;
+        }
+
+        public Builder setEqId(int eqId) {
+            this.eqId = eqId;
+            return this;
+        }
+
+
+        public Builder copy(Supplier supplier) {
+            this.supId = supplier.supId;
+            this.supName = supplier.supName;
+            this.eqId = supplier.eqId;
+            return this;
+        }
+
+        public Supplier build() {
+
+            return new Supplier(this);
+        }
+    }
+}
---
+++ src/main/java/za/ac/cput/factory/WorkOutFactory.java
@@ -0,0 +1,11 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.entity.WorkOut;
+
+public class WorkOutFactory {
+
+    public static WorkOut createWorkOut(int workoutId, String workoutName, String workoutDescription) {
+        return new WorkOut.Builder().setWorkoutId(workoutId).setWorkoutName(workoutName)
+                .setWorkoutDescription(workoutDescription).build();
+    }
+}
