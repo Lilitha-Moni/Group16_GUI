3080f101174b964319694d4b48a465e7bccdcf85
DrMpahleni <216205999@mycput.ac.za>
2021-10-20 21:31:43 +0200

    payment

--- src/main/java/za/ac/cput/entity/Admin.java
+++ src/main/java/za/ac/cput/entity/Admin.java
@@ -1,19 +1,27 @@
 package za.ac.cput.entity;
 
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /* Admin.java
 Entity for the Admin
 Author: Bongisa Mpahleni (216205999)
 Date: 09 June 2021
 */
-
+@Entity
+@Table(name="Admin")
 public class Admin {
+
+    @Id
+    private int ID;
 
     private String fisrtName;
     private String lastName;
     private String phoneNo;
     private String emailAddress;
-    private int ID;
+
 
     public Admin(Builder builder) {
         this.ID = builder.ID;
@@ -21,6 +29,10 @@ public class Admin {
         this.lastName = builder.lastName;
         this.phoneNo = builder.phoneNo;
         this.emailAddress = builder.emailAddress;
+    }
+
+    public Admin() {
+
     }
 
     public String getFisrtName() {
@@ -54,9 +66,10 @@ public class Admin {
                 '}';
     }
 
-    public static class Builder {
+    public static class Builder
+    {
 
-        private String firstName, lastName, phoneNo, emailAddress;
+        private String firstName,lastName,phoneNo,emailAddress;
         private int ID;
 
         public Builder setID(int ID) {
@@ -65,41 +78,47 @@ public class Admin {
         }
 
         //Set Firstname
-        public Builder setFirstName(String firstName) {
-            this.firstName = firstName;
+        public Builder setFirstName(String firstName)
+        {
+            this.firstName= firstName;
             return this;
         }
-
         //Set Lastname
-        public Builder setLastName(String lastName) {
-            this.lastName = lastName;
+        public Builder setLastName(String lastName)
+        {
+            this.lastName= lastName;
             return this;
         }
 
         //Set PhoneNumber
-        public Builder setPhoneNo(String phoneNo) {
-            this.phoneNo = phoneNo;
+        public Builder setPhoneNo(String phoneNo)
+        {
+            this.phoneNo= phoneNo;
             return this;
         }
 
         //Set EmailAddress
-        public Builder setEmailAddress(String emailAddress) {
-            this.emailAddress = emailAddress;
+        public Builder setEmailAddress(String emailAddress)
+        {
+            this.emailAddress= emailAddress;
             return this;
         }
 
 
-        public Admin build() {
+
+        public Admin build()
+        {
             return new Admin(this);
         }
 
         //Copy method
-        public Builder copy(Admin admin) {
+        public Builder copy(Admin admin)
+        {
             this.ID = admin.ID;
-            this.firstName = admin.fisrtName;
-            this.lastName = admin.lastName;
-            this.phoneNo = admin.phoneNo;
-            this.emailAddress = admin.emailAddress;
+            this.firstName= admin.fisrtName;
+            this.lastName=admin.lastName;
+            this.phoneNo=admin.phoneNo;
+            this.emailAddress=admin.emailAddress;
 
 
             return this;
